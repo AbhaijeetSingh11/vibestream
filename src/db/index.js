9b8275ff63a8fc4bab3cfa7c796ae97350b911c8
ABHAIJEET SINGH <abhaijeet11@gmail.com>
2025-12-03 23:57:07 +0530

    database connection done and file updation

---
+++ src/db/index.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+import {DB_NAME} from '../constants.js';
+
+const connectDB = async()=>{
+    try {
+        const connection = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+        console.log(`Database connected successfully and hosted on ${connection.connection.host}`);
+
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+}
+export default connectDB;
